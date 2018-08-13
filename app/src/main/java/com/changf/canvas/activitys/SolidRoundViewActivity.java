5ac5585f807cde52c94ad6e4b17dc9a717450ce1
dxp2020 <dxp3327844>
2018-08-14 00:51:50 +0800

    1、新增练手CheckView

--- app/src/main/java/com/changf/canvas/activitys/SolidRoundViewActivity.java
+++ app/src/main/java/com/changf/canvas/activitys/SolidRoundViewActivity.java
@@ -2,14 +2,25 @@ package com.changf.canvas.activitys;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.changf.canvas.R;
+import com.changf.canvas.view.CheckView;
 
 public class SolidRoundViewActivity  extends Activity {
+    private CheckView cv_check_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_solid_round_view);
+
+        cv_check_view = findViewById(R.id.cv_check_view);
+        cv_check_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cv_check_view.check();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/changf/canvas/view/CheckView.java
@@ -0,0 +1,155 @@
+package com.changf.canvas.view;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.Handler;
+import android.os.Message;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.changf.canvas.R;
+
+/**
+ * <ul type="disc">
+ * <li>Author: Sloop</li>
+ * <li>Version: v1.0.0</li>
+ * <li>Copyright: Copyright (c) 2015</li>
+ * <li>Date: 2016/2/5</li>
+ * <li><a href="http://www.sloop.icoc.cc"    target="_blank">作者网站</a>      </li>
+ * <li><a href="http://weibo.com/5459430586" target="_blank">作者微博</a>      </li>
+ * <li><a href="https://github.com/GcsSloop" target="_blank">作者GitHub</a>   </li>
+ * </ul>
+ */
+public class CheckView extends View {
+
+    private static final int ANIM_NULL = 0;         //动画状态-没有
+    private static final int ANIM_CHECK = 1;        //动画状态-开启
+    private static final int ANIM_UNCHECK = 2;      //动画状态-结束
+
+    private Context mContext;           // 上下文
+    private int mWidth, mHeight;        // 宽高
+    private Handler mHandler;           // handler
+
+    private Paint mPaint;
+    private Bitmap okBitmap;
+
+    private int animCurrentPage = 0;       // 当前页码
+    private int animMaxPage = 0;           // 总页数
+    private int animDuration = 500;         // 动画时长
+    private int animState = ANIM_UNCHECK;      // 动画状态
+
+    private boolean isCheck = false;        // 是否只选中状态
+
+    public CheckView(Context context) {
+        super(context, null);
+
+    }
+
+    public CheckView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    /**
+     * 初始化
+     * @param context
+     */
+    private void init(Context context) {
+        mContext = context;
+
+        mPaint = new Paint();
+        mPaint.setColor(0xffFF5317);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setAntiAlias(true);
+
+        okBitmap = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.checkmark);
+
+        animMaxPage = okBitmap.getWidth()/okBitmap.getHeight()-1;
+
+        mHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                if (animCurrentPage < animMaxPage && animCurrentPage >= 0) {
+                    invalidate();
+                    if (animState == ANIM_NULL)
+                        return;
+                    if (animState == ANIM_CHECK) {
+                        animCurrentPage++;
+                    } else if (animState == ANIM_UNCHECK) {
+                        animCurrentPage--;
+                    }
+                }
+            }
+        };
+    }
+
+
+    /**
+     * View大小确定
+     * @param w
+     * @param h
+     * @param oldw
+     * @param oldh
+     */
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = w;
+        mHeight = h;
+    }
+
+    /**
+     * 绘制内容
+     * @param canvas
+     */
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // 移动坐标系到画布中央
+        canvas.translate(mWidth / 2, mHeight / 2);
+
+        // 绘制背景圆形
+        canvas.drawCircle(0, 0, 240, mPaint);
+
+        // 得出图像边长
+        int sideLength = okBitmap.getHeight();
+
+        // 得到图像选区 和 实际绘制位置
+        Rect src = new Rect(sideLength * animCurrentPage, 0, sideLength * (animCurrentPage + 1), sideLength);
+        Rect dst = new Rect(-200, -200, 200, 200);
+
+        // 绘制
+        canvas.drawBitmap(okBitmap, src, dst, null);
+    }
+
+    private boolean isPositiveSequence = true;
+
+    /**
+     * 选择
+     */
+    public void check() {
+        if(isPositiveSequence&&animCurrentPage==12){
+            isPositiveSequence = false;
+            animCurrentPage--;
+        }else if(!isPositiveSequence&&animCurrentPage==0){
+            isPositiveSequence = true;
+        }
+        if(isPositiveSequence&&animCurrentPage<12){
+            animState = ANIM_CHECK;
+            mHandler.sendEmptyMessageDelayed(0, animDuration / animMaxPage);
+            isCheck = true;
+        }else{
+            animState = ANIM_UNCHECK;
+            mHandler.sendEmptyMessageDelayed(0, animDuration / animMaxPage);
+            isCheck = false;
+        }
+    }
+
+
+}
